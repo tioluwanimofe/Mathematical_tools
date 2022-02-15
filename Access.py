533ad165ee3f67bdc5fd31a98297e5af50b27d22
Mofe_Sanya <86200511+tioluwanimofe@users.noreply.github.com>
2022-02-15 08:26:32 +0100

    I'm just having fun :)

---
+++ Access.py
@@ -0,0 +1,11 @@
+print("Please enter Passcode: ")
+text = ("Tioluwanimofe", "Mofe", "mofe", "Adesanya", "adesanya", "Boss")
+
+word = input()
+
+s = text
+
+if text == word:
+    print("Access allowed")
+else:
+    print("Access allowed")
