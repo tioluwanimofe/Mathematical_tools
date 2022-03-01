529cddc748c10adcc6c790fab5b7421b56421102
Mofe_Sanya <86200511+tioluwanimofe@users.noreply.github.com>
2022-03-01 22:37:39 +0100

    Update Unit Converter.py

--- Unit Converter.py
+++ Unit Converter.py
@@ -1,6 +1,6 @@
 # Code to look for the specific units in whatever text is inputted so that the program would run
 # whatever code assigned to that unit
-print("This is a product of BOTS of tommorrow, which is solely owned by Adesanya Tioluwanimofe." \n "Do ensure to endorse the code. Thank you")
+print("Input the numerical values")
 print("Please first enter the unit to be converted from then enter the unit to be converted to")
 print("Use \n cm- Centimeter \n m- Meter \n c- Celsius \n f- Fahrenheit \n d- USD dollar \n n- Naira")
 
@@ -14,15 +14,17 @@ def temperature():
     f = 9 / 5
     a = f * celsius
     h = a + 32
-    print(h)
+    r = str(h)
+    print("The value in fahrenheit is " + r)
 
 
 def temperature1():
     fahrenheit = int(input())
     c = fahrenheit - 32
     e = c * 5
-    s = e / 9
-    print(s)
+    i = e / 9
+    s = str(i)
+    print("The value in celsius is " + s)
 
 
 # For updated version, pip a currency library that will continuously import the current currency value
@@ -30,28 +32,32 @@ def currency():
     naira = int(input())
     d = 415.87
     o = naira / d
-    print(o)
+    a = str(o)
+    print("The value in dollar is " + a)
 
 
 def currency1():
     dollar = int(input())
     n = 415.87
-    o = dollar * n
-    print(o)
+    a = dollar * n
+    i = str(a)
+    print("The value in naira is " + i)
 
 
 def length1():
     meter = int(input())
     c = 100
     e = meter / c
-    print(e)
+    n = str(e)
+    print('The value in meter is ' + n)
 
 
 def length():
     centimeter = int(input())
     m = 100
     e = centimeter * m
-    print(e)
+    t = str(e)
+    print("The value in centimeter is " + t)
 
 
 # Algorithm to check for the option picked
@@ -63,7 +69,7 @@ elif Unit1 == "cm" and Unit2 == "m":
     length1()
 elif Unit1 == "m" and Unit2 == "cm":
     length()
-elif Unit1 == "d" and Unit2 == "m":
+elif Unit1 == "d" and Unit2 == "n":
     currency1()
 elif Unit1 == "n" and Unit2 == "d":
     currency()
